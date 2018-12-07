09ee96b7c2eb1050111fc0a0c096332482d3fdc0
GaryHassan <39998912+GaryHassan@users.noreply.github.com>
2018-12-07 16:15:35 +0100

    Add files via upload

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBL5WUmY6U5Wx4lC9D6EzpVzA0-7rfUw1U",
+    authDomain: "ma-chat-box-12d81.firebaseapp.com",
+    databaseURL: "https://ma-chat-box-12d81.firebaseio.com",
+});
+
+export default base;
---
+++ src/components/Connexion.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Connexion extends React.Component {
+
+	goToChat = event => {
+		event.preventDefault();
+		// On récupère le pseudo
+		const pseudo = this.pseudoInput.value;
+		// On change d'url
+		this.context.router.transitionTo(`/pseudo/${pseudo}`);
+	}
+
+	render() {
+		return (
+			<div className="connexionBox">
+				<form className="connexion" onSubmit={(e) => this.goToChat(e)} >
+					<input type="text" placeholder="Pseudo" required ref={(input) => { this.pseudoInput = input}} />
+					<button type="submit">GO</button>
+				</form>
+			</div>
+		)
+	}
+
+	static contextTypes = {
+		router: React.PropTypes.object
+	};
+}
+
+//On exporte afin de l'importer dans un autre fichier
+export default Connexion;
